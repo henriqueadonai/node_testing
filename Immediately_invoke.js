46fef2320438eed2fab49cee3e12e863ba11dbfb
Henrique Cabral <henrique.cabral@appcentrica.com>
2017-06-07 09:30:45 -0400

    Add immediately

---
+++ Immediately_invoke.js
@@ -0,0 +1,24 @@
+
+// Function Declaration
+function Dosomething_function(){
+    return 'DoSomething Function'    ;
+}
+
+
+// Function Expression
+var doSomething_Exp = function(){
+    return 'DoSomething_exp';
+}
+
+//Immediately-Invoked Function Expression Syntax
+(function DoSomething_Immediately(){
+    return "DoSomething_Immediately";
+}) ();
+
+
+(function superSecret(){
+  var superSecret = 195;
+})()
+
+// This allows us to use variables and data within functions, while still keeping those variables and data private from prying eyes.
+
