1688fb3b96a60956dc8b9647d03ba855b2a08566
Henrique Cabral <henrique.cabral@appcentrica.com>
2017-06-22 15:44:21 -0400

    Reorganize files

---
+++ JS_INFO/why_does_3_true_is_4.js
@@ -0,0 +1,23 @@
+
+
+console.log('3+true = 4')
+console.log(3 + true == 4)
+
+console.log("false === 0");
+console.log( false === 0);
+
+console.log("console equal 48")
+console.log('4'+ 8)
+
+console.log("Equal 1")
+console.log(true + false)
+
+console.log("1 + 1 + '1' equal 21")
+console.log(1+1+ '1')
+
+console.log("'1' + 1 + 1 equal 111")
+console.log('1'+1+1)
+
+
+console.log("Console log -'69' + 69 equals 0 ")
+console.log(-'69' + 69)
